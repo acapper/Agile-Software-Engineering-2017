0ba2d909fb1957b64d08e2a18cd2c1746d21dec0
Brian Hung <brianhung96@gmail.com>
2018-02-13 15:50:51 +0000

    test classes

---
+++ Agile 2018.Tests/UnitTestLoginDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Agile_2018;
+
+namespace Agile_2018.Tests
+{
+    [TestClass]
+    public class UnitTestLoginDatabase
+    {
+        [TestMethod]
+        public void Login()
+        {
+            //open connection            
+
+            Assert.IsTrue(true); //use login function to return true
+        }
+    }
+}
