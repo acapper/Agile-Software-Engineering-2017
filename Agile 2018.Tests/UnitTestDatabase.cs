867bba1d4813daa8db5bc113954774c1a248f241
Brian Hung <brianhung96@gmail.com>
2018-02-13 15:50:51 +0000

    test classes

---
+++ Agile 2018.Tests/UnitTestDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Agile_2018;
+
+namespace Agile_2018.Tests
+{
+    [TestClass]
+    public class UnitTestDatabaseConnection
+    {
+        //Test for connection to database
+        [TestMethod]
+        public void DatabaseConnectionTest()
+        {
+            //open connection
+
+            Assert.IsTrue(true);    //insert sql command to select some data
+                }
+    }
+}
