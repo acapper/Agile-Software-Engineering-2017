029f71eae1b63391502092cc03700f94e925effe
antonycapper <antonycapper@labpc061>
2018-02-14 14:58:55 +0000

    Updated unit test and uplaod file

--- Agile 2018.Tests/DatabaseFileHandlerTests.cs
+++ Agile 2018.Tests/DatabaseFileHandlerTests.cs
@@ -11,37 +11,12 @@ namespace Agile_2018.Tests
         [TestMethod]
         public void UploadFile()
         {
-            Stream myStream = null;
             DatabaseFileHandler dfh = new DatabaseFileHandler();
 
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-
-            openFileDialog1.InitialDirectory = "c:\\";
-            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 2;
-            openFileDialog1.RestoreDirectory = true;
-
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    if ((myStream = openFileDialog1.OpenFile()) != null)
-                    {
-                        using (myStream)
-                        {
-                            // Insert code to read the stream here.
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
-                }
-            }
-
-            String path = openFileDialog1.FileName;
-
+            String path = "C:\\windows-version.txt";
             dfh.UploadFile(path);
+
+            Assert.IsTrue(false, path);
         }
     }
 }
--- Agile 2018/DatabaseFileHandler.cs
+++ Agile 2018/DatabaseFileHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Windows.Forms;
 
 namespace Agile_2018
 {
@@ -9,7 +11,37 @@ namespace Agile_2018
     {
         public void UploadFile(string path)
         {
-            throw new NotImplementedException();
+            /*Stream myStream = null;
+            DatabaseFileHandler dfh = new DatabaseFileHandler();
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "C:";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            String path = "";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            path = openFileDialog1.FileName;
+                            Console.WriteLine(path);
+                            //dfh.UploadFile(path);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }*/
         }
     }
 }
