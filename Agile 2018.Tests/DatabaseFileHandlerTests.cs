11cbc50356c9f948f61b92cfe69c608665db4662
antonycapper <antonycapper@labpc061>
2018-02-14 14:05:18 +0000

    Made upload file unit test

---
+++ Agile 2018.Tests/DatabaseFileHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Agile_2018.Tests
+{
+    [TestClass]
+    public class DatabaseFileHandlerTests
+    {
+        [TestMethod]
+        public void UploadFile()
+        {
+            Stream myStream = null;
+            DatabaseFileHandler dfh = new DatabaseFileHandler();
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+            String path = openFileDialog1.FileName;
+
+            dfh.UploadFile(path);
+        }
+    }
+}
---
+++ Agile 2018/DatabaseFileHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agile_2018
+{
+    public class DatabaseFileHandler
+    {
+        public void UploadFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
