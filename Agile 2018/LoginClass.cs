1a8a2b4d5f75af818dfa69c1bd087da7f3dd281a
csaking <kingconor@live.co.uk>
2018-02-14 12:08:28 +0000

    updated LoginClass to work

--- Agile 2018/LoginClass.cs
+++ Agile 2018/LoginClass.cs
@@ -9,19 +9,23 @@ namespace Agile_2018
 {
     public class LoginClass
     {
-        //open connection
-        ConnectionClass.OpenConnection();
-        SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM logindetails WHERE staffID ='" + StaffID + "' AND password='" + Password + "'", con);
-        /* in above line the program is selecting the whole data from table and the matching it with the user name and password provided by user. */
-        DataTable result = new DataTable(); //this is creating a virtual table  
-        sda.Fill(result);
-        if (dt.Rows[0][0].ToString() == "1")        //when login matches
+        public void MyMethod(string StaffID, string Password)
         {
-            //...........
-        }  
-        else{
-        Console.Write("Invalid username or password");
+            //open connection
+            ConnectionClass.OpenConnection();
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM logindetails WHERE staffID ='" + StaffID + "' AND password='" + Password + "'", ConnectionClass.con);
+            /* in above line the program is selecting the whole data from table and the matching it with the user name and password provided by user. */
+            DataTable result = new DataTable(); //this is creating a virtual table  
+            sda.Fill(result);
+            if (result.Rows[0][0].ToString() == "1")        //when login matches
+            {
+                //...........
             }
-        ConnectionClass.CloseConnection();
-}
+            else
+            {
+                Console.Write("Invalid username or password");
+            }
+            ConnectionClass.CloseConnection();
+        }
+    }
 }
