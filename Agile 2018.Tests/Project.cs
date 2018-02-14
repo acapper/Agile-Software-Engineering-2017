61301407e60d340d1b449747e387b08a771e5bfd
dylanmcleish <dylanmcleish@labpc074>
2018-02-14 14:40:12 +0000

    Added comments to unit test

--- Agile 2018.Tests/Project.cs
+++ Agile 2018.Tests/Project.cs
@@ -4,11 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Agile_2018.Tests
 {
     [TestClass]
-    public class UnitTest2
+    public class Project
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod()
         {
+
+
+            //get user input (ID of Project to view)
+            int idToView = 1;
+            //SQL statement which queries the database for this project based on this input
+
+            //Return the results from this SQL query
+            //Display these results to the user
+
+
         }
     }
 }
