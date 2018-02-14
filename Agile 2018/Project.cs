7db6e224e76d6679781813c8b1156377542bdbc4
dylanmcleish <dylanmcleish@labpc074>
2018-02-14 15:23:38 +0000

    Added Database connection to Project.cs

--- Agile 2018/Project.cs
+++ Agile 2018/Project.cs
@@ -8,5 +8,30 @@ namespace Agile_2018
     public class Project
     {
 
+        int idToView = 0;
+        
+        public int viewProject(int input)
+        {
+            ConnectionClass.OpenConnection();
+
+            idToView = input;
+
+            
+
+
+
+
+
+
+
+            return idToView;
+            
+
+        }
+        
+
+
+
+
     }
 }
