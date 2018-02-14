636ff9a4fd25c8ed85067a30d3eaced3966e9288
dlzkelly <dlzkelly@dundee.ac.uk>
2018-02-14 13:58:29 +0400

    Created Project class and unit test.

---
+++ Agile 2018.Tests/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agile_2018.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
