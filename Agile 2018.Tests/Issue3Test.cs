a14da0262974a3150974b47555d64395d5b57cc0
petermccartney <petermccartney@LABPC071>
2018-02-14 14:54:53 +0000

    14/02 changes.

---
+++ Agile 2018.Tests/Issue3Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Agile_2018;
+
+namespace Agile_2018.Tests
+{
+    [TestClass]
+    public class Issue3Test
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Issue3 Issue3 = new Issue3();
+            string teststring = "Testsdbasdhasjk";
+            //Issue3.SubmitDB(teststring);
+            //We put a record in
+            //Check if record is in (Use display project method)
+            //If the record title matches our given title
+            //Pass
+            Assert.IsTrue(teststring == Issue3.SubmitDB(teststring));
+
+
+            //Assert.AreEqual(2, r, "Result is equal to 2");
+        }
+    }
+}
--- Agile 2018/Issue3.cs
+++ Agile 2018/Issue3.cs
@@ -2,14 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
 
 namespace Agile_2018
 {
     public class Issue3
     {
-        public int AddNumbers(int v1, int v2)
+        //make a connection string
+        
+        
+        public string SubmitDB(string name)
         {
-            return v1 + v2;
+            string ConnectString = "Server = silva.computing.dundee.ac.uk;Database=17agileteam5db;Uid=17agileteam5;Pwd=7485.at5.5847";
+            Debug.WriteLine("Here");
+            MySqlConnection connection = new MySqlConnection(ConnectString);
+            MySqlCommand cmd;
+            connection.Open();
+            try
+            {
+                cmd = connection.CreateCommand();
+                cmd.CommandText = "INSERT INTO projects(Title)VALUES('"+name+"')";
+                Debug.WriteLine("Entered");
+                return name;
+            }
+            catch(Exception)
+            {
+                Debug.WriteLine("Exception");
+                throw;
+            }
+            finally
+            {
+                if(connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
         }
     }
 }
