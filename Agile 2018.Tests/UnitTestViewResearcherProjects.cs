dff568c6aeaae17a586ea5987ab0b9f6b44c45fb
Steven Macgregor (Student) <SZMacgregor@dundee.ac.uk>
2018-02-16 18:20:30 +0000

    Issue#13 - Created branch. Created Stored procedure and confirmed work with DB. Created testcase - returns true regardless after addition of method - Need to check. Created viewProjects method - Need to check.

---
+++ Agile 2018.Tests/UnitTestViewResearcherProjects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Agile_2018;
+
+namespace Agile_2018.Tests
+{
+    [TestClass]
+    public class UnitTestViewResearcherProjects
+    {
+        [TestMethod]
+        public void TestViewResearcherProjects()
+        {
+            string staffID = "sweeb";
+            ViewProjects testView = new ViewProjects();
+            int i = testView.ViewResearcherProjects(staffID);
+            Assert.IsNotNull(i);
+        }
+    }
+}
