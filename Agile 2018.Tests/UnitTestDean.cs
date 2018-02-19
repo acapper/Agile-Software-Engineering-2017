40484088f6d69d6f818bea081b21743c77487cc8
Brian Hung <brianhung96@gmail.com>
2018-02-19 12:36:30 +0000

    Added dean reject test

---
+++ Agile 2018.Tests/UnitTestDean.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Agile_2018;
+
+namespace Agile_2018.Tests
+{
+    [TestClass]
+    public class UnitTestDean
+    {
+        [TestMethod]
+        public void TestRejectDean()
+        {
+            Dean testDean = new Dean();
+            int i = testDean.Reject(50);
+            Assert.AreEqual(1, i);
+        }
+    }
+}
