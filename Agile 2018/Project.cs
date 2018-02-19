b1274c2da0f8d465c039b025ebd8797da3c56038
petermccartney <petermccartney@LABPC071>
2018-02-19 15:59:26 +0000

    Records that are added will now also add to the linking table.
    The function adds a new project titled : AnotherTest to the projects table. Gets the uniqiue ID for this project and adds that id along with a default user id : 15 to the userproject link table.
    
    ALL VARIABLE MENTIONED ARE IN TEST

--- Agile 2018/Project.cs
+++ Agile 2018/Project.cs
@@ -5,14 +5,15 @@ using System.Web;
 using MySql.Data.MySqlClient;
 using System.Diagnostics;
 
+
 namespace Agile_2018
 {
     public class Project
     {
         //make a connection string
-        
-        
-        public bool CreateProject(string title)
+
+
+        public bool CreateProject(string title, int userID)
         {
             MySqlCommand cmd;
             ConnectionClass.OpenConnection();
@@ -20,11 +21,37 @@ namespace Agile_2018
 
             try
             {
+                //FIRST INSERT THE NEW PROJECT INTO THE PROJECTS TABLE
                 //SQL Query
                 cmd.CommandText = "INSERT INTO projects(Title)VALUES(@title)";
 
                 // Populate SQl query values
                 cmd.Parameters.AddWithValue("@title", title);
+
+                // Execute Query
+                cmd.ExecuteNonQuery();
+
+                //query server to find out what ID that record got
+                cmd.CommandText = "SELECT ProjectID From projects Where Title = @newtitle";
+                cmd.Parameters.AddWithValue("@newtitle", title);
+                //Read the return and grab the HIGHEST project ID. This allows multiple of the same named records
+                string projID = "";
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    projID = reader["ProjectID"].ToString();
+                  
+                }
+                reader.Close();
+
+                //FOLLOW BY INSERTING THE PROJECT AND ID INTO LINK
+                //SQL Query
+                cmd.CommandText = "INSERT INTO userprojectpairing(LoginDetails_UserID,Projects_ProjectID)VALUES(@userID,@projID)";
+
+                // Populate SQl query values
+                cmd.Parameters.AddWithValue("@userID", userID);
+                cmd.Parameters.AddWithValue("@projID", projID);
+               
 
                 // Execute Query
                 cmd.ExecuteNonQuery();
