523301704a56d72807598ccd4019a6131b881d2e
Brian Hung <brianhung96@gmail.com>
2018-02-21 14:15:22 +0000

    Added RIS reject test

---
+++ Agile 2018.Tests/UnitTestRIS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Agile_2018;
+
+namespace Agile_2018.Tests
+{
+    [TestClass]
+    public class UnitTestRIS
+    {
+        [TestMethod]
+        public void TestRejectDean()
+        {
+            RIS testRIS = new RIS();
+            int i = testRIS.RISReject(50);
+            Assert.AreEqual(1, i);
+        }
+    }
+}
