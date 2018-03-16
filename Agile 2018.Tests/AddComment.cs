240aa1e3e9185f693d98ae1ec0ed85b8f38dfd34
petermccartney <petermccartney@LABPC071>
2018-03-16 14:44:13 +0000

    comments added

---
+++ Agile 2018.Tests/AddComment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Agile_2018;
+using MySql.Data.MySqlClient;
+
+namespace Agile_2018.Tests
+{
+    [TestClass]
+    public class AddComment
+    {
+        [TestMethod]
+        public void commentAddTest()
+        {
+            //pass through string
+            string comment = "This is my comment";
+            int projectID = 530;
+            Comment myComment = new Comment();
+            Assert.AreEqual(1, myComment.postComment(comment,projectID));
+        }
+
+        [TestMethod]
+        public void getCommentTest()
+        {
+            //return expected string
+            //string comes from default id
+            string comment = "This is my comment";
+            int projectID = 530;
+            Comment myComment = new Comment();
+            Assert.AreEqual(comment, myComment.getComment(projectID));
+        }
+    }
+}
